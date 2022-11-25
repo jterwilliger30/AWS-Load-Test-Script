8262f3e279f1e28102d984e250ed652dfb3703f6
Jack Terwilliger <71192591+jterwilliger30@users.noreply.github.com>
2022-11-24 20:17:08 -0800

    Add files via upload

---
+++ load_test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome("chromedriver")
+password = ""
+
+while True:
+    driver.get("https://prod.indigo.fakeflickr.biz")
+    driver.add_cookie({'name': '', 'value': ''})
+    time.sleep(.15)
+
